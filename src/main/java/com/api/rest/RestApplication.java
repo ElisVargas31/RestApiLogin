f3306d88726b6b57053a11f13a5bce6bcb0f2b99
ElisVargas <vargaselis1031@gmail.com>
2024-11-29 19:36:12 -0500

    ultimos cambios

--- src/main/java/com/api/rest/RestApplication.java
+++ src/main/java/com/api/rest/RestApplication.java
@@ -3,6 +3,7 @@ package com.api.rest;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
@@ -15,4 +16,9 @@ public class RestApplication extends SpringBootServletInitializer  {
 		SpringApplication.run(RestApplication.class, args);
 	}
 
+	
+	    @Override
+	    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+	        return application.sources(RestApplication.class);
+	    }
 }
